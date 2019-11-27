f0ed07d910c3c944d9002e731ec58966356fc866
ashya <ashyamanning@pursuit.org>
2019-11-26 20:51:32 -0500

    complete problem 1 of part one

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://official-joke-api.appspot.com/random_ten").then(res => {
+        if (!res.ok) {
+            throw Error(res.statusText);
+        }
+        return res.json();
+    }).then(jokes => {
+        let ul = document.querySelector("ul");
+        jokes.forEach(joke => {
+            let li = document.createElement("li");
+            li.innerText = joke.setup;
+            ul.appendChild(li);
+        })
+    }).catch(err => {
+        console.log(err);
+    })
+})
